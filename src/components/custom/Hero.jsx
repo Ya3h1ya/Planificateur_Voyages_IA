df2c5bf401aeaa822461feaf2fe14e68a82a1839
Ya3h1ya <yahya.aboussaad2001@gmail.com>
2024-12-10 13:07:33 +0100

    Message décrivant les changements apportés

---
+++ src/components/custom/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    
+    <div className='flex items-center mx-56 gap-9'>
+        <h1
+        className='font-extrabold text-[60px] text-center'>
+            Découvrez Votre Prochaine Aventure Avec l'IA 
+         </h1>
+         
+    </div>
+  )
+}
+
+export default Hero
--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RouterProvider,createBrowserRouter } from 'react-router-dom'
-
+import CreateTrip from './create-trip/index.jsx'
+import Header from './components/custom/Header'
 
 const router=createBrowserRouter([
   {
     path:'/',
     element:<App/>
+  },{
+    path:'/create-trip',
+    element:<CreateTrip/>
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header/>
     <RouterProvider router={router}/>
   </React.StrictMode>,
 )
