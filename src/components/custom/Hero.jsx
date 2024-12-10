7add6a4b14f567b63b0dcf730f385c06ea47dae1
Ya3h1ya <yahya.aboussaad2001@gmail.com>
2024-12-10 15:30:33 +0100

    Message décrivant les changements apportés

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
     
-    <div className='flex items-center mx-56 gap-9'>
+    <div className='flex flex-colitems-center mx-56 gap-9'>
         <h1
-        className='font-extrabold text-[60px] text-center'>
-            Découvrez Votre Prochaine Aventure Avec l'IA 
-         </h1>
+        className='font-extrabold text-[50px] text-center mt-16'>
+            <span className='text-[#f56551]'>Planificateur De Voyage</span><br></br>Découvrez Votre Prochaine Aventure Avec l'IA 
+        <p className='text-xl text-gray-500 text-center'>Votre planificateur de voyage personnel et créateur d'itinéraires, élaborant des programmes sur mesure en fonction de vos intérêts et de votre budget.</p>
+        <Link to={'/create-trip'}>
+        <Button>COMMENCER - C'est gratuit !</Button>
+        </Link>
+        </h1>
+         
          
     </div>
   )
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 
 function createTrip() {
   return (
-    <div>createTrip</div>
+    <div className='sm:px-10 md:px-31 lg:px-56 xl:px-10 px-5 mt-10' >
+      <h2 className='font-bold text-3xl text-center'>Dites-nous vos préférences de voyage.</h2>
+        <p className='mt-3 text-gray-500 text-xl'>Fournissez simplement quelques informations de base, et notre planificateur de voyage générera un itinéraire personnalisé en fonction de vos préférences.</p>      
+      
+          <div className='mt-20'>
+             <div>
+              <h2 className='text-xl my-3 font-medium'>Quelle est votre destination préférée ?</h2>
+              <GooglePlacesAutocomplete
+              apiKey={import.meta.env.VITE_GOOGLE_PLACES_API_KEY}
+              
+                />
+             </div>
+          </div>
+      </div>
   )
 }
 
